f8d4aead77f86bf36eb6a00ec199c0b4c168a71d
Krzysztof Nowak <info@knowak.net>
2018-04-20 23:18:37 +0200

    Update Crime dto

--- app/src/main/java/com/bignerdranch/android/criminalintent/dto/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/dto/Crime.java
@@ -17,4 +17,32 @@ public class Crime {
         mID = UUID.randomUUID();
         mDate = new Date();
     }
+
+    public UUID getID() {
+        return mID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
 }
